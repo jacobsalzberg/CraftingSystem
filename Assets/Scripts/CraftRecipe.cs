a7928a231da0029d9fbaa90085cd2fb25df52305
Jacob Salzberg <jacob1818@gmail.com>
2021-01-29 15:56:20 -0300

    2 - Recipe Blueprints

---
+++ Assets/Scripts/CraftRecipe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//blueprint (class) for the craftrecipe
+public class CraftRecipe
+{
+    //array of items
+    public int[] requiredItems;
+    public int itemToCraft;
+
+    public CraftRecipe(int itemToCraft, int[] requiredItems)
+    {
+        this.requiredItems = requiredItems;
+        this.itemToCraft = itemToCraft;
+    }
+}
