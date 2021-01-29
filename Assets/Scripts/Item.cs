79ed895bcb95f9b72421125a7d64586369db9c40
Jacob Salzberg <jacob1818@gmail.com>
2021-01-29 10:22:14 -0300

    1 - Items and Constructors

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public int id;
+    public string title;
+    public string description;
+    public Sprite icon;
+    //new object from the dictionary constructor
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+    public Item(int id, string title, string description, Sprite icon, Dictionary<string, int> stats)
+    {
+        //instance field equals the paramenter passed in
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        //this.icon = icon;
+        this.icon = Resources.Load<Sprite>("Items/" + title); ;
+        this.stats = stats;
+    }
+
+    // Clone Creator
+    public Item(Item item)
+    {
+        this.id = item.id;
+        this.title = item.title;
+        this.description = item.description;
+        this.icon = item.icon;
+        this.stats = item.stats;
+    }
+}
